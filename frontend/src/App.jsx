5f7e48540def0fab0c7859064ada8e1f1351b974
Keval-Hingu <kevalhinguo0019@gmail.com>
2025-07-26 18:56:16 +0530

    Navbar and theme switching is done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,25 @@
+
 import Navbar from './components/Navbar.jsx';
-import { Routes, Route} from "react-router-dom";
+
 import HomePage from './pages/HomePage.jsx';
 import ProductPage from './pages/ProductPage.jsx';
-function App() {
-  return (
-    <div>
-      <Navbar />
 
+import { Routes, Route} from "react-router-dom";
+import { useThemeStore } from './store/useThemeStore.js';
+
+import { Toaster } from "react-hot-toast";
+
+
+function App() {
+  const  {theme} = useThemeStore();
+  return (
+    <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme={theme}>
+      <Navbar/>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/products/:id' element={<ProductPage/>}/>
       </Routes>
-      
+      <Toaster/>
     </div>
   )
 }
