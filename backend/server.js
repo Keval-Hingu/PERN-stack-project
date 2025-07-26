b9649f5958667b997358745849156264e08b7bbb
Keval-Hingu <kevalhinguo0019@gmail.com>
2025-07-26 11:42:17 +0530

    Rate limiting and bot detection set

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 
 import { sql } from "./config/db.js";
+import { aj } from "./lib/arcjet.js";
+
 // import routes :
 import productRoutes from "./routes/products.routes.js";
 
@@ -20,14 +22,36 @@ app.use(helmet()); // helmet is a security middleware that helps to protect our 
 app.use(morgan("dev")); // log the request.
 app.use(cors());
 
+// apply arcjet rate-limit to all routes
+app.use(async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, {
+      requested: 1, // specifies that each request consumes 1 token
+    });
 
-// initial setup :
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.status(429).json({ error: "Too Many Requests" });
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({ error: "Bot access denied" });
+      } else {
+        res.status(403).json({ error: "Forbidden" });
+      }
+      return;
+    }
 
-// app.get("/test" ,(req, res)=>{
-//     res.send ("Hello from PERN Stack");
-//     console.log("test Route is accessed");
-//     // console.log(res.getHeaders()); // limited information is provided without helmet
-// })
+    // check for spoofed bots
+    if (decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())) {
+      res.status(403).json({ error: "Spoofed bot detected" });
+      return;
+    }
+
+    next();
+  } catch (error) {
+    console.log("Arcjet error", error);
+    next(error);
+  }
+});
 
 // APIs :
 
